d79ed53000a2ad97ab22c9e5f953f1f1e9f20a01
AndriiBai <andriy.bay@gmail.com>
2019-07-15 09:40:38 +0300

    add block01

--- templates/goals.php
+++ templates/goals.php
@@ -3,28 +3,28 @@
         <h2 class="title">Goals</h2>
         <div class="goals__content">
             <div class="goals__box">
-                <i class="mercury-icon-screen goals__icon" aria-hidden="true"></i>
+                <i class="icon mercury-icon-user goals__icon" aria-hidden="true"></i>
                 <div class="goals__text">
                     <div class="goals__title">Lorem Ipsum</div>
                     <div class="goals__descr">Lorem Ipsum is simply dummy text the printing and typeseing industry Lorem Ipsum has been standard.Lorem Ipsum is simply dummy text the printing</div>
                 </div>
             </div>
             <div class="goals__box">
-                <i class="mercury-icon-group goals__icon" aria-hidden="true"></i>
+                <i class="icon mercury-icon-gear goals__icon" aria-hidden="true"></i>
                 <div class="goals__text">
                     <div class="goals__title">Lorem Ipsum</div>
                     <div class="goals__descr">Lorem Ipsum is simply dummy text the printing and typeseing industry Lorem Ipsum has been standard. Lorem Ipsum has been standard</div>
                 </div>
             </div>
             <div class="goals__box">
-                <i class="mercury-icon-chart-up goals__icon" aria-hidden="true"></i>
+                <i class="mercury-icon-target-2 goals__icon" aria-hidden="true"></i>
                 <div class="goals__text">
                     <div class="goals__title">Lorem Ipsum</div>
                     <div class="goals__descr">Lorem Ipsum is simply dummy text the printing and typeseing industry Lorem Ipsum has been standard. Lorem Ipsum is simply dummy text the printing</div>
                 </div>
             </div>
             <div class="goals__box">
-                <i class="mercury-icon-target-2 goals__icon" aria-hidden="true"></i>
+                <i class="mercury-icon-lightbulb-gears goals__icon" aria-hidden="true"></i>
                 <div class="goals__text">
                     <div class="goals__title">Lorem Ipsum</div>
                     <div class="goals__descr">Lorem Ipsum is simply dummy text the printing and typeseing industry Lorem Ipsum has been standard. Lorem Ipsum is simply dummy text the printing</div>
--- templates/services.php
+++ templates/services.php
@@ -1,4 +1,12 @@
 <section id="services" class="services">
+
+    <div class="section-wave section-wave-white">
+        <svg x="0px" y="0px" viewBox="0 0 1920 45" width="1920" height="45px" preserveAspectRatio="none">
+            <style type="text/css"></style>
+            <path d="M1920,0c-82.8,0-108.8,44.4-192,44.4c-78.8,0-116.5-43.7-192-43.7c-77.1,0-115.9,44.4-192,44.4c-78.2,0-114.6-44.4-192-44.4c-78.4,0-115.3,44.4-192,44.4C883.1,45,841,0.6,768,0.6C691,0.6,652.8,45,576,45C502.4,45,461.9,0.6,385,0.6C306.5,0.6,267.9,45,191,45C115.1,45,78,0.6,0,0.6V45h1920V0z"></path>
+        </svg>
+    </div>
+
     <div class="wrapper">
         <h2 class="title">Our Services</h2>
         <div class="services__content">
@@ -6,19 +14,19 @@
             <div class="services__list">
 
                 <div class="services__item">
-                    <i class="mercury-icon-gears services__icon"></i>
+                    <i class="mercury-icon-note services__icon"></i>
                     <div class="services__name">Lorem ipsum</div>
                     <div class="services__descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
                 </div>
 
                 <div class="services__item">
-                    <i class="mercury-icon-lib services__icon"></i>
+                    <i class="mercury-icon-desktop-chart services__icon"></i>
                     <div class="services__name">Lorem ipsum</div>
                     <div class="services__descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
                 </div>
 
                 <div class="services__item">
-                    <i class="mercury-icon-partners services__icon"></i>
+                    <i class="mercury-icon-chart services__icon"></i>
                     <div class="services__name">Lorem ipsum</div>
                     <div class="services__descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
                 </div>
