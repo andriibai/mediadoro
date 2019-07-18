20e1f2abbea99fb02eabc54bd50a315253e2bef6
AndriiBai <andriy.bay@gmail.com>
2019-07-18 10:06:39 +0300

    ad favicon and replace block

--- templates/team.php
+++ templates/team.php
@@ -1,35 +1,16 @@
 <section id="team" class="team">
+
+    <div class="circle-bg"></div>
+    <div class="circle-bg-2"></div>
+
     <div class="wrapper">
         <h2 class="title">Our team</h2>
         <div class="team__content">
-
             <div class="team__info">
-                <img src="assets/prod/img/team-1.jpg" alt="">
-                <div class="team__caption">
-                    <p class="team__name">Lorem ipsum</p>
-                    <div class="divider"></div>
-                    <p class="team__position">Lorem ipsum</p>
-                </div>
+                <div class="team__title">Lorem ipsum</div>
+                <div class="team__text">Lorem Ipsum is simply dummy text the printing and typeseing industry Lorem Ipsum has been standard.Lorem Ipsum is simply dummy text the printing and typeseing industry Lorem Ipsum has been standard.Lorem Ipsum is simply dummy text the printing and typeseing industry Lorem Ipsum has been standard.Lorem Ipsum is simply dummy text the printing and typeseing industry Lorem Ipsum has been standard.</div>
             </div>
-
-            <div class="team__info">
-                <img src="assets/prod/img/team-2.jpg" alt="">
-                <div class="team__caption">
-                    <p class="team__name">Lorem ipsum</p>
-                    <div class="divider"></div>
-                    <p class="team__position">Lorem ipsum</p>
-                </div>
-            </div>
-
-            <div class="team__info">
-                <img src="assets/prod/img/team-3.jpg" alt="">
-                <div class="team__caption">
-                    <p class="team__name">Lorem ipsum</p>
-                    <div class="divider"></div>
-                    <p class="team__position">Lorem ipsum</p>
-                </div>
-            </div>
-
+            <img class="team__img" src="assets/prod/img/team.jpg" alt="">
         </div>
     </div>
 </section>
